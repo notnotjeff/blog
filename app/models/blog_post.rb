5e4db4929cc756b8ff5bddaf8894116e7780c605
Jeff Craig <jeffcraig35@gmail.com>
2023-05-09 15:17:10 -0400

    Add blog post model methods with tests

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,18 +4,18 @@ class BlogPost < ApplicationRecord
 
   scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
-  scope :scheduled, -> { where('published_at > ?', Time.current) }
   scope :published, -> { where('published_at <= ?', Time.current) }
+  scope :scheduled, -> { where('published_at > ?', Time.current) }
 
   def draft?
     published_at.nil?
   end
 
-  def scheduled?
-    published_at? && published_at > Time.current
-  end
-
   def published?
     published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
   end
 end
